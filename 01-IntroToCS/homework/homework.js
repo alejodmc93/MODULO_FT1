2719c15d5c1c9180ef9bfcf953a14e9acdff7278
alejodmc93 <fmendozala@gmail.com>
2023-01-25 17:59:04 -0500

    RESUELTO

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,37 @@
 'use strict';
 
-function BinarioADecimal(num) {hola}
+function BinarioADecimal(num) {
+   let num1 = num +"";
+   let numArray = num1.split("");
 
-function DecimalABinario(num) {}
+   let nuevoNumero = numArray.reverse();
+
+   let suma = 0 ;
+
+   for(let i = 0 ; i < nuevoNumero.length ; i++){
+    suma = Math.pow(2,i)*parseInt(nuevoNumero[i])+suma
+   }
+
+   return suma;   
+}
+
+function DecimalABinario(num) {
+    if(num === 1){
+      return 1;
+  }
+  
+ let numeros = [];
+
+ let residuo = ""
+
+ while(num > 1){
+  residuo = (num % 2 +"")+residuo;
+  num = Math.floor(num/2); 
+  
+  numeros.push(num);
+  
+}
+}
 
 module.exports = {
    BinarioADecimal,
