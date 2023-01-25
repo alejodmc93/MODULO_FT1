ca8d6ecf2cda59015c34f77fe1e3dd068684506d
alejodmc93 <fmendozala@gmail.com>
2023-01-25 18:24:18 -0500

    RESUELTO

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,18 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let array = [1];
+  let cont = 2;
+  while(num > 1){
+    if(num % cont ===0){
+      num = num / cont;
+      array.push(cont)
+    }
+    else{
+      cont = cont + 1;
+    }
+  }
+  return array;
 }
 
 function bubbleSort(array) {
@@ -14,7 +25,18 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  let arrayOrdenado = array;
 
+  for(var i = 0 ; i < array.length -1 ; i++){
+      for(var j = 0 ; j < array.length +1 ;j++){
+        if(array[j] > array[j+1]){
+          var aux = array[j];
+          array[j]=array[j+1]; 
+          array[j+1] = aux
+        }
+      }
+  }
+  return arrayOrdenado;
 }
 
 
@@ -23,7 +45,19 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for(let i = 0 ; i < array.length ; i++){
+    let j = i-1;
+    let aux = array[i];
 
+    while(j >= 0 && aux < array[j]){
+      array[j+1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = aux 
+  }
+
+  return array;
 }
 
 
@@ -32,7 +66,20 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 0 ; i < array.length;i++){
+    let minIndex = i;
+    for(let j = i + 1 ; j < array.length;j++ ){
+      if(array[j] < array[minIndex]){
+        minIndex = j;
+      }
+    }
+    if(i !== minIndex){
+      let aux = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = aux;
+    }
+  }
+  return array;
 }
 
 
