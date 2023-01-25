03e73e56e66e4cd7cad5bbca0cce16cc702cd98b
alejodmc93 <fmendozala@gmail.com>
2023-01-25 18:26:26 -0500

    RESUELTO

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,30 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if(array.length < 2){
+    return array
+  }
+
+  let left = [];
+  let right = [];
+  let igual = [];
+
+  let pivot = array[Math.floor(Math.random()*array.length)];
+  console.log(pivot)
+
+  for(let i = 0 ; i < array.length ; i++){
+    if(array[i] < pivot){
+      left.push(array[i]);
+    }
+    else if(array[i] > pivot){
+      right.push(array[i]);
+    }
+    else{
+      igual.push(array[i]);
+    }
+  }
+  return quickSort(left).concat(igual).concat(quickSort(right));
+
 }
 
 function mergeSort(array) {
@@ -16,6 +40,22 @@ function mergeSort(array) {
   // Tu código:
 
   //okey probemos ahora
+  if(array.length < 2){
+    return array
+  } 
+
+  let mitad = Math.floor(array.length/2);
+  let right = array.slice(mitad);
+  let left = array.slice(0,mitad);
+
+  let rightArray = mergeSort(right);
+  let leftArray = mergeSort(left); 
+
+  let resultado = [];
+
+  while(left.length && right.length){
+    
+  }
 }
 
 // No modificar nada debajo de esta línea
